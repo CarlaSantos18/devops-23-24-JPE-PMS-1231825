635f107206303e1200c92540b4de37d484244aa1
Carla Santos <1231825@isep.ipp.pt>
2024-03-07 10:25:43 +0000

    Added an email field to employee and changed IllegalArgumentException to InstantiationException in the constructor. Closes #3

--- CA1/basic/src/main/java/com/greglturnquist/payroll/DatabaseLoader.java
+++ CA1/basic/src/main/java/com/greglturnquist/payroll/DatabaseLoader.java
@@ -35,8 +35,8 @@ public class DatabaseLoader implements CommandLineRunner { // <2>
 
 	@Override
 	public void run(String... strings) throws Exception { // <4>
-		this.repository.save(new Employee("Frodo", "Baggins", "ring bearer",2));
-		this.repository.save(new Employee("Bilbo", "Baggins", "burglar",4));
+		this.repository.save(new Employee("Frodo", "Baggins", "ring bearer",2, "frodobaggins@shire.com"));
+		this.repository.save(new Employee("Bilbo", "Baggins", "burglar",4, "bilbobaggins@shire.com"));
 	}
 }
 // end::code[]
